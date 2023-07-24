db9e2f714a8385d2e290666aa1684cba8c442e28
sathiyakumars2308 <sathiyakumars@gmail.com>
2023-07-24 11:14:51 +0530

    added C# folder

--- Java/Sample/src/test/java/TestClass.java
+++ Java/Sample/src/test/java/TestClass.java
@@ -9,12 +9,26 @@ public class TestClass {
     @Test
     public void sampleTest()
     {
- System.out.println(SampleEnum.valueOf("FRIDAY"));
-        System.out.println(SampleEnum.values());
 
- System.out.println((SampleEnum.FRIDAY));
-        System.out.println((SampleEnum.FRIDAY.getWeekDay()));
+        SampleEnum SE = SampleEnum.valueOf("FRIDAY");
+        System.out.println("SE :"+SE);
+        System.out.println("SE.name() :"+SE.name());
 
-        System.out.println((SampleEnum.FRIDAY.getDay()));
+        System.out.println("SE.getDay() :"+SE.getDay());
+        System.out.println("SE.getWeekDay() :"+SE.getWeekDay());
+
+        System.out.println("SampleEnum.valueOf(\"FRIDAY\") :"+SampleEnum.valueOf("FRIDAY"));
+
+        System.out.println("SampleEnum.values() :"+SampleEnum.values());
+
+
+ System.out.println("SampleEnum.FRIDAY :"+SampleEnum.FRIDAY);
+
+        System.out.println("SampleEnum.FRIDAY.name():"+SampleEnum.FRIDAY.name());
+        System.out.println("(SampleEnum.FRIDAY.getWeekDay()) :"+(SampleEnum.FRIDAY.getWeekDay()));
+
+        System.out.println("(SampleEnum.FRIDAY.getDay()) :"+(SampleEnum.FRIDAY.getDay()));
+
+
     }
 }
